8e6d9a6f987ba9191a1ca2a910ea4fec486f769a
Billy Ho <choose0125@gmail.com>
2022-06-06 17:53:08 -0400

    sesason-f

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,37 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        // This is the only time we do direct assigment
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: '' };
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(   
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
+    
 
-    //react says  we have to define render  
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
-        return <div>Loading...</div>;
+        return <Spinner message = "Please accpet location request"/>;
+    }
+
+
+    //react says  we have to define render  
+    render() {
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
